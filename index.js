e982525a6632e3936440882f7f051fe6894b432f
zarc1411 <zarc1483@gmail.com>
2021-01-02 11:20:51 +0530

    Removed all the console logs

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const cors = require("cors");
 app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
-console.log("hello world");
 
 const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
-console.log(url);
-console.log(typeof url);
 
 mongoose.connect(url, {
   useNewUrlParser: true,
@@ -38,7 +35,6 @@ const Month = mongoose.model("Month", monthSchema);
 
 app.put(`/months/`, (request, response) => {
   const linkToAdd = request.body;
-  console.log("This is the body", linkToAdd);
   const link = new Link({
     tag: linkToAdd.tag,
     name: linkToAdd.name,
@@ -52,7 +48,6 @@ app.put(`/months/`, (request, response) => {
       return linksArray.save();
     })
     .then((result) => {
-      console.log(result);
       response.status(200).end();
     })
     .catch((error) => next(error));
@@ -61,7 +56,6 @@ app.put(`/months/`, (request, response) => {
 app.get(`/months/:month`, (request, response, next) => {
   Month.find({ monthName: `${request.params.month}` })
     .then((result) => {
-      console.log(result);
       if (result.length > 0) {
         response.json(result);
       } else response.status(304).end();
@@ -75,9 +69,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log("Server running on port 3001");
-});
+app.listen(PORT, () => {});
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
